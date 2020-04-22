d733199de6e5edf7c497a73d4b86ecae6d68f4e6
hellocxx <30309630+hellocxx@users.noreply.github.com>
2020-04-22 17:09:28 +0800

    4.22 3

--- Run.py
+++ Run.py
@@ -1,2 +1,13 @@
 #this is the main demo!!!
 #PyQt5 + QTdesigner + PyUic + PyRcc + Pyinstaller
+
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import MainWinbuddyAndsunxv
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = MainWinbuddyAndsunxv.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
