78a29f5e7f55c051f28f45ebe3d1051bb7ee4b09
hellocxx <30309630+hellocxx@users.noreply.github.com>
2020-04-23 09:59:53 +0800

    4.23 signal and slot

--- Run.py
+++ Run.py
@@ -3,11 +3,11 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
-import MainWinbuddyAndsunxv
+import MainWinSignalAndSlot
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = MainWinbuddyAndsunxv.Ui_MainWindow()
+    ui = MainWinSignalAndSlot.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
